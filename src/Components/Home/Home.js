3fef00c7405fcc3cb4f62d3e4fc0674cb95f5f72
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-23 00:28:33 +0600

    home section updated using react firebase

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,29 @@
-import React from "react";
+//normal firebase way
+/* import React from "react";
 import useFirebase from "../../hooks/useFirebase";
 
 const Home = () => {
   const { user } = useFirebase();
+  return (
+    <div>
+      <h2>this is home</h2>
+      <p>Current ser: {user ? user.displayName : "No Body"} </p>
+    </div>
+  );
+};
+
+export default Home; */
+
+//firebase using react firebase
+
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
+const Home = () => {
+  const [user] = useAuthState(auth);
   return (
     <div>
       <h2>this is home</h2>
