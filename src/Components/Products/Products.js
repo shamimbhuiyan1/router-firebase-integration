3e0cc984a3770ee2b5e2a3cda32db55956f5a13b
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-23 00:07:33 +0600

    home section updated using react firebase

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,4 +1,6 @@
-import React from "react";
+//normal firebase way
+
+/* import React from "react";
 import useFirebase from "../../hooks/useFirebase";
 
 const Products = () => {
@@ -7,7 +9,28 @@ const Products = () => {
     <div>
       <h2>Heree are all the product</h2>
       <p> {user ? user.email : "No User"}</p>
-      <h4>{user ? user.displayName : "No User"} </h4>
+      <h5>{user ? user.displayName : "No User"} </h5>
+    </div>
+  );
+};
+
+export default Products; */
+
+//firebase using react firebase
+
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
+const Products = () => {
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <h2>Heree are all the product</h2>
+      <p> {user ? user.email : "No User"}</p>
+      <h5>{user ? user.displayName : "No User"} </h5>
     </div>
   );
 };
