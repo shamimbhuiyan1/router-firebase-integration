cf62ee96850f80c1f87173bd188f4f47f1dd29f1
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-22 23:58:18 +0600

    header section updated using react firebase

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,6 @@
-import React from "react";
+//normal way
+
+/* import React from "react";
 import { Link } from "react-router-dom";
 import useFirebase from "../../hooks/useFirebase";
 import "./Header.css";
@@ -14,6 +16,38 @@ const Header = () => {
         <span>{user?.displayName && user.displayName}</span>
         {user?.uid ? (
           <button onClick={handleSignOut}>Sign Out</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default Header; */
+
+//using react firebase
+
+import { getAuth, signOut } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import app from "../../firebase.init";
+import "./Header.css";
+
+const auth = getAuth(app);
+const Header = () => {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="header">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/register">Register</Link>
+        <span>{user?.displayName && user.displayName}</span>
+        {user?.uid ? (
+          <button onClick={() => signOut(auth)}>Sign Out</button>
         ) : (
           <Link to="/login">Login</Link>
         )}
